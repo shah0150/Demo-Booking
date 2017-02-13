3e3583b5d592b28b2f3aab73197eefc1b0e6883d
shah0150 <shah0150@algonquinlive.com>
2017-02-13 17:24:47 -0500

    Custom Help Menu

--- app/src/main/java/com/shah0150algonquinlive/demobooking/MainActivity.java
+++ app/src/main/java/com/shah0150algonquinlive/demobooking/MainActivity.java
@@ -1,22 +1,38 @@
 package com.shah0150algonquinlive.demobooking;
 
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity implements NavigationDrawerFragment
         .NavigationDrawerCallbacks {
-
+    private AboutDialogFragment mAboutDialog;
+    private static final String ABOUT_DIALOG_TAG;
     private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    static {
+        ABOUT_DIALOG_TAG = "About Dialog";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mAboutDialog = new AboutDialogFragment();
+        if (savedInstanceState == null) {
+            F1.newInstance().show(getFragmentManager(), null);
+        }
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
 
@@ -63,5 +79,62 @@ public class MainActivity extends AppCompatActivity implements NavigationDrawerF
                 break;
         }
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        if (id == R.id.action_settings) {
+            DialogFragment newFragment = new AboutDialogFragment();
+            newFragment.show(getFragmentManager(), ABOUT_DIALOG_TAG);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class F1 extends DialogFragment {
+
+        public static F1 newInstance() {
+            F1 f1 = new F1();
+            f1.setStyle(DialogFragment.STYLE_NO_FRAME, android.R.style.Theme_DeviceDefault_Dialog);
+            return f1;
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            // Remove the default background
+            getDialog().getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            // Inflate the new view with margins and background
+            View v = inflater.inflate(R.layout.popup_layout, container, false);
+
+            // Set up a click listener to dismiss the popup if they click outside
+            // of the background view
+            v.findViewById(R.id.popup_root).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dismiss();
+                }
+            });
+
+            return v;
+        }
     }
 }
